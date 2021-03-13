0e50bcac1caa05b8537da31a35cb5e154ad05a64
Siritkam <danidoom@yandex.ru>
2021-03-13 04:05:07 +0300

    на проверку

--- app/server.py
+++ app/server.py
@@ -1,17 +1,20 @@
-from flask import render_template
 from flask import Flask
+from flask import render_template, redirect
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/login')
 def login():
-    return render_template('auth.html')
+    return render_template('login.html')
 
 
-@app.route('/error')
+@app.route('/')
 def Error():
-    return render_template('error.html')
+    return redirect ('/login')
+    #return render_template('error.html')    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
